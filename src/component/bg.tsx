339dad08bd5f1f3ea57bb29ed3206392d8094134
Yeonji-Noob <rhcp3885@naver.com>
2023-04-07 07:41:40 +0900

    background 추가

---
+++ src/component/bg.tsx
@@ -0,0 +1,33 @@
+import Videobgpc from "../imgs/bg/background-web_ver.mp4";
+import styled from "styled-components";
+
+const Bg_Container = styled.div`
+width: 100%;
+height: 100%;
+background-color: #fff09a;
+`;
+
+
+const VideoBg = styled.video`
+position: absolute;
+width: 100%;
+height: 100%;
+object-fit: fill;
+`;
+
+const BackGround = () => {
+    return (
+
+        <div className="bg">
+            <Bg_Container>
+                <VideoBg className="pc" src={Videobgpc} playsInline muted autoPlay loop width="100%" height="100%">
+                </VideoBg>
+                {/* <video className="mb" src="./imgs/bg/background-mobile_ver.mp4" playsInline autoPlay loop></video> */}
+            </Bg_Container>
+
+        </div>
+
+    );
+}
+
+export default BackGround;
---
+++ src/styles/global_style.tsx
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+
+
+// resetCSS랑 같은 역할
+const GlobalStyle = createGlobalStyle`
+  
+  
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Helvetica", "Arial", sans-serif;
+    line-height: 1.5;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+  }
+
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body {
+    font-family: 'ms_sans_serif';
+  }
+`;
+
+export default GlobalStyle;
