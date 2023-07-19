da57b5238500e5a1c64f29dbff0189c440cb0422
Yeonji-Noob <rhcp3885@naver.com>
2023-07-19 12:37:49 +0900

    이미지 폴더 정리

--- src/component/hamster/hamster-icon.tsx
+++ src/component/hamster/hamster-icon.tsx
@@ -1,3 +1,4 @@
+import { useState, useContext, useMemo } from "react";
 import { create } from "zustand";
 import { ComponentsProps } from "../interface";
 import icon4 from "../../imgs/icons/hamster_icon.png";
@@ -13,27 +14,8 @@ export const useHamsterStore = create<HamsterStore>(set => ({
 
 export const Hamster = ({ DivIconContainer, Icon, Icon1Label }: ComponentsProps) => {
 
-
   const isHamster = useHamsterStore(state => state.isHamster);
   const setHamster = useHamsterStore(state => state.setHamster);
-
-
-  let touchStartTime = 0;
-  let touchEndTime = 0;
-
-  const handleTouchStart = () => {
-    touchStartTime = new Date().getTime();
-  };
-
-  const handleTouchEnd = () => {
-    touchEndTime = new Date().getTime();
-    const touchDuration = touchEndTime - touchStartTime;
-
-    if (touchDuration < 300) {
-      // 300밀리초보다 작은 경우 가볍게 터치로 인식
-      ChangeHamster();
-    }
-  };
 
 
   const ChangeHamster = () => {
@@ -47,8 +29,7 @@ export const Hamster = ({ DivIconContainer, Icon, Icon1Label }: ComponentsProps)
       left="10px"
       style={{ cursor: `url(https://cur.cursors-4u.net/nature/nat-1/nat21.png), auto` }}
       onClick={ChangeHamster}
-      onTouchStart={handleTouchStart}
-      onTouchEnd={handleTouchEnd}
+      onTouchEnd={ChangeHamster}
     >
       <Icon src={icon4} top="15px" left="13px" alt="hamster-icon">
       </Icon>
--- src/containers/bg.tsx
+++ src/containers/bg.tsx
@@ -1,10 +1,6 @@
-// import Videobgpc from "../imgs/bg/background-web_ver.mp4";
-// import Videobgpc2 from "../imgs/bg/background-web2_ver.mp4";
-
 //gif가 제일 용량이 작아서 채택
 import VideobgpcGif from "../imgs/bg/background-web_ver.gif";
 import Videobgpc2Gif from "../imgs/bg/background-web2_ver.gif"
-// import Videobgmb from "../imgs/bg/background-mobile_ver.mp4";
 import styled from "styled-components";
 import { useHamsterStore } from "../component/hamster/hamster-icon";
 
@@ -39,15 +35,10 @@ const BackGround: React.FC = () => {
 
     <BgContainer>
       <GifBg className="pc" src={VideobgpcGif}
-        // playsInline muted autoPlay loop 
-        // width="100%" height="100%"
         style={{ display: isHamster ? 'none' : 'block' }}>
       </GifBg>
-      {/* <MobileVideoBg className="mb" src={Videobgmb} playsInline muted autoPlay loop width="100%" height="100%">
-      </MobileVideoBg> */}
+
       <GifBg className="pc2" src={Videobgpc2Gif}
-        // playsInline muted autoPlay loop 
-        // width="100%" height="100%"
         style={{ display: isHamster ? 'block' : 'none' }}
       >
       </GifBg>
